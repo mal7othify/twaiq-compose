86454e8c31db9af9fc98df9f950e79c4bc2a3580
Maryam Alhuthayfi <mal7othify@gmail.com>
2022-06-22 21:01:32 +0300

    4th day - 3rd week

--- app/src/main/java/com/example/maryam/ui/imageslist/ImagesScreen.kt
+++ app/src/main/java/com/example/maryam/ui/imageslist/ImagesScreen.kt
@@ -2,16 +2,19 @@ package com.example.maryam.ui.imageslist
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.GridItemSpan
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -22,21 +25,27 @@ import com.example.maryam.ImageCard
 fun ImagesScreen(viewModel: ImagesViewModel = hiltViewModel()) {
   val state = viewModel.state.value
   Surface(
-    modifier = Modifier.fillMaxSize(),
-    color = MaterialTheme.colors.secondary
+    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.secondary
   ) {
-    LazyVerticalGrid(
-      cells = GridCells.Adaptive(150.dp),
-      modifier = Modifier.padding(16.dp)
-    ) {
-      item(
-        span = { GridItemSpan(4) }
+    if (state.isLoading || state.error.isNotEmpty()) {
+      Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
       ) {
-        Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ")
+        if (state.isLoading) CircularProgressIndicator()
+        if (state.error.isNotEmpty()) Text(text = state.error)
       }
-      items(state.images) { picture ->
-        Log.e("ImagesScreen", "${state.images}")
-        ImageCard(image = picture)
+    } else {
+      LazyVerticalGrid(
+        cells = GridCells.Adaptive(150.dp), modifier = Modifier.padding(16.dp)
+      ) {
+        item(span = { GridItemSpan(4) }) {
+          Text(text = "My lovely app")
+        }
+        items(state.images) { picture ->
+          Log.e("ImagesScreen", "${state.images}")
+          ImageCard(image = picture)
+        }
       }
     }
   }
